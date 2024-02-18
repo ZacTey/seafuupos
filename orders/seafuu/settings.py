a88440f94f340f58aa1fd151babf4b3a2c1d8283
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2024-02-18 11:58:03 +0800

    Update settings.py

--- orders/seafuu/settings.py
+++ orders/seafuu/settings.py
@@ -60,7 +60,7 @@ TINYMCE_DEFAULT_CONFIG = {
 # Application definition
 
 INSTALLED_APPS = [
-    'orders.apps.OrdersConfig',
+    'apps.OrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,8 +68,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'tinymce',
-    'whitenoise.runserver_nostatic',
+    'tinymce'
 ]
 
 MIDDLEWARE = [
