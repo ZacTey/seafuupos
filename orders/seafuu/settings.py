5b71b77c88accdc1a28e15194b5036a6f989e055
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2024-02-18 14:21:10 +0800

    Update settings.py

--- orders/seafuu/settings.py
+++ orders/seafuu/settings.py
@@ -60,7 +60,7 @@ TINYMCE_DEFAULT_CONFIG = {
 # Application definition
 
 INSTALLED_APPS = [
-    'orders.apps.OrdersConfig',
+    'apps.OrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
